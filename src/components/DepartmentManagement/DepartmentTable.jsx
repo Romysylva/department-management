c2ba416ee25b76d7856f418109f9efca12d189c8
Promise Sylva <sylvapromise5@gmail.com>
2025-09-29 00:31:02 +0100

    import path update

--- src/components/DepartmentManagement/DepartmentTable.jsx
+++ src/components/DepartmentManagement/DepartmentTable.jsx
@@ -1,5 +1,5 @@
 import { ArrowDownIcon } from "lucide-react";
-import { Button } from "../ui/button";
+import { Button } from "../ui/Button";
 
 export const DepartmentTable = ({ departments, onManage }) => {
   return (
