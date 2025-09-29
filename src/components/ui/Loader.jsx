f11c677d9323f3e94ed5a3f5c337d07b5b56ce93
Promise Sylva <sylvapromise5@gmail.com>
2025-09-29 12:44:44 +0100

    update loader

--- src/components/ui/Loader.jsx
+++ src/components/ui/Loader.jsx
@@ -9,16 +9,6 @@ export const Spinner = () => {
   );
 };
 
-export const SkeletonLoader = () => {
-  return (
-    <div className="animate-pulse space-y-4">
-      <div className="h-4 bg-gray-300 rounded w-3/4"></div>
-      <div className="h-4 bg-gray-300 rounded w-5/6"></div>
-      <div className="h-4 bg-gray-300 rounded w-2/3"></div>
-    </div>
-  );
-};
-
 export const ProgressBarLoader = () => {
   return (
     <div className="w-full h-1 bg-gray-200 overflow-hidden">
